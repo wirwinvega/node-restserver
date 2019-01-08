ccce6be5250eb04f14c10db24adf7f435df4ef88
Irving <irving@bitfx.mx>
2019-01-08 09:26:43 -0600

    First commit

---
+++ server/server.js
@@ -0,0 +1,47 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => {
+    res.json('Get usuario');
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if (body.name === undefined) {
+        res.status(400).json({
+            ok: false,
+            message: 'Field name is required.'
+        })
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
+});
+
+app.put('/usuario', (req, res) => {
+    res.json('Put usuario');
+});
+
+app.patch('/usuario', (req, res) => {
+    res.json('Patch usuario');
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('Delete usuario');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando el puerto: ${process.env.PORT}`);
+});
